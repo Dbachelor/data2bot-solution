776386e419c94c27c733b2dd5b6d3126e72000a4
Dbachelor <84332279+Dbachelor@users.noreply.github.com>
2023-08-04 12:53:42 +0100

    first commit

---
+++ data_reader.py
@@ -0,0 +1,51 @@
+import json
+
+class DataReader:
+    
+    def run(self, sourceFile="data_1.json", destinationFile="schema_1.json"):
+        print(1)
+        data = self.fileToJson("data/"+sourceFile)
+        output = self.processData(data)
+        self.writeTofile("schema/"+destinationFile, output)
+        return 1
+        
+
+
+    def fileToJson(self, path):
+        f = open(path)
+        data = json.load(f)
+        return data
+
+    def processData(self, data):
+        final_output = {}
+        for item in data['message']:
+            output = {}
+            data_type = ''
+            
+            match self.getDataType(data['message'][item]):
+                case 'dict':
+                    data_type = 'enum'
+                case 'list':
+                    data_type = 'array'
+                case 'str':
+                    data_type = 'string'
+                case 'int' | 'float':
+                    data_type = 'integer'
+                case 'bool':
+                    data_type = 'boolean'
+            output = {item:{"type": data_type, "tag": "", "description": "","required": False}}
+            final_output.update(output)
+        return final_output
+                    
+        
+    def getDataType(self,item):
+        ss = str(type(item))
+        d = ss.split("'")[1]
+        print(d, item)
+        return d.strip()
+        
+    def writeTofile(self, path, data):
+        f = open(path, "w")
+        #f.write(json.dumps(data))
+        json.dump(data, f, indent = 4)
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from data_reader import DataReader
+
+readData = DataReader()
+
+readData.run("data_2.json", "schema_2.json")
